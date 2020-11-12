0eadb296dbf95895d92037cfe7bf3c6763288f1a
MatteoMenichetti <73056858+MatteoMenichetti@users.noreply.github.com>
2020-11-12 13:44:25 +0100

    Add files via upload

---
+++ SpiralMatrix/Test/tests/SpiralMatrixTests.java
@@ -0,0 +1,85 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import spiral.SpiralMatrix;
+
+public class SpiralMatrixTests {
+	SpiralMatrix sm;
+	int[][] m, t;
+
+	@Test
+	public void test2same2() {
+		sm = new SpiralMatrix(2);
+		m = sm.calculate();
+		t = new int[][] { { 1, 2 }, { 4, 3 } };
+		assertTrue(m[0].length == t[0].length && m.length == t.length && cycle(m, t));
+	}
+
+	@Test
+	public void test4same4() {
+		sm = new SpiralMatrix(4);
+		m = sm.calculate();
+		t = new int[][] { { 1, 2, 3, 4 }, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 } };
+		// assertTrue(m[0].length == t[0].length && m.length == t.length && cycle(m,
+		// t));
+		assertTrue(m[0].length == t[0].length);
+		assertTrue(m.length == t.length);
+		assertTrue(cycle(m, t));
+	}
+
+	@Test
+	public void test1same1() {
+		sm = new SpiralMatrix(1);
+		t = new int[][] { { 1 } };
+		m = sm.calculate();
+		assertTrue(m[0].length == t[0].length);
+		assertTrue(m.length == t.length);
+		assertTrue(cycle(m, t));
+	}
+
+	@Test
+	public void test1Line() {
+		sm = new SpiralMatrix(1, 5);
+		t = new int[][] { { 1, 2, 3, 4, 5 } };
+		m = sm.calculate();
+		assertTrue(m[0].length == t[0].length);
+		assertTrue(m.length == t.length);
+		assertTrue(cycle(m, t));
+	}
+
+	private boolean cycle(int[][] m, int[][] t) {
+		for (int i = 0; i < m.length; i++)
+			for (int j = 0; j < m[0].length; j++)
+				if (m[i][j] != t[i][j])
+					return false;
+		return true;
+	}
+
+	@Test
+	public void test3same3() {
+		sm = new SpiralMatrix(3);
+		m = sm.calculate();
+		t = new int[][] { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
+		assertTrue(m[0].length == t[0].length);
+		assertTrue(m.length == t.length);
+		assertTrue(cycle(m, t));
+	}
+
+	@Test
+	public void testCalculateNminorM() {
+		sm = new SpiralMatrix(4, 5);
+		m = sm.calculate();
+		t = new int[][] { { 1, 2, 3, 4, 5 }, { 14, 15, 16, 17, 6 }, { 13, 20, 19, 18, 7 }, { 12, 11, 10, 9, 8 } };
+		assertTrue(m[0].length == t[0].length && m.length == t.length && cycle(m, t));
+	}
+
+	@Test
+	public void testCalculateNmajorM() {
+		sm = new SpiralMatrix(5, 4);
+		m = sm.calculate();
+		t = new int[][] { { 1, 2, 3, 4 }, { 14, 15, 16, 5 }, { 13, 20, 17, 6 }, { 12, 19, 18, 7 }, { 11, 10, 9, 8 } };
+		assertTrue(m[0].length == t[0].length && m.length == t.length && cycle(m, t));
+	}
+}
---
+++ SpiralMatrix/src/spiral/SpiralMatrix.java
@@ -0,0 +1,91 @@
+package spiral;
+
+public class SpiralMatrix {
+
+	private int n;
+	private int m;
+
+	public SpiralMatrix(int n, int m) {
+		this.n = n;
+		this.m = m;
+	}
+
+	/*
+	 * 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+	 */
+	public SpiralMatrix(int n) {
+		this.m = this.n = n;
+	}
+
+	public int[][] calculate() {
+		int[][] t = new int[n][m];
+		build(t, 0, 0);
+		return t;
+	}
+
+	private void build(int[][] t, int n, int m) {
+		if (t[n][m] == 0) {
+			int[] p = right(t, n, m);
+			if (t.length > 1 && t[p[0] + 1][p[1]] == 0) {
+				p = down(t, p[0] + 1, p[1]);
+				p = left(t, p[0], p[1] - 1);
+				if (t.length > 2) {
+					p = up(t, p[0] - 1, p[1]);
+					if (p[0] > 0 && p[0] < t[0].length - 1 && p[1] < t.length - 1) {
+						/* guardare */if (t[p[0]][p[1]] == 0)
+							build(t, p[0], p[1]);
+					}
+				}
+			}
+		}
+	}
+
+	private int[] up(int[][] t, int n, int m) {
+		if (t[n][m] == 0) {
+			t[n][m] = t[n + 1][m] + 1;
+			return up(t, n - 1, m);
+		} else
+			return new int[] { n + 1, m + 1 };
+	}
+
+	private int[] left(int[][] t, int n, int m) {
+		if (m >= 0 && t[n][m] == 0) {
+			t[n][m] = t[n][m + 1] + 1;
+			return left(t, n, m - 1);
+		}
+		return new int[] { n, m + 1 };
+	}
+
+	private int[] right(int[][] t, int n, int m) {
+		if (n == 0 && m == 0) {
+			t[0][0] = 1;
+			return right(t, 0, 1);
+		}
+		if (m < this.m - 1) {
+			if (m > 0 && t[n][m - 1] != 0) {
+				t[n][m] = t[n][m - 1] + 1;
+			} else if (n > 0)
+				t[n][m] = t[n - 1][m] + 1;
+			if (t[n][m + 1] == 0)
+				return right(t, n, m + 1);
+		}
+		if (m == this.m - 1) {
+			this.n--;
+			t[n][m] = t[n][m - 1] + 1;
+			return right(t, n, m + 1);
+		}
+		return new int[] { n, m - 1 };
+	}
+
+	private int[] down(int[][] t, int n, int m) {
+		if (t[n][m] != 0)
+			;
+		if (n < this.n && t[n][m] == 0) {
+			t[n][m] = t[n - 1][m] + 1;
+			return down(t, n + 1, m);
+		}
+		t[n][m] = t[n - 1][m] + 1;
+		this.m--;
+		return new int[] { n, m };
+	}
+}
